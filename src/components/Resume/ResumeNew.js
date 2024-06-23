6a8fcfbae122dc90eda1c3a0045dd4bba9b4e72e
Dell <ayushpatel20@gnu.ac.in>
2024-06-23 10:29:57 +0530

    Download Button Changed

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
 import pdf from "../../Assets/../Assets/Ayush_Resume.pdf";
-import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
+import "./ResumeButton.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
@@ -18,15 +18,33 @@ function ResumeNew() {
     <div>
       <Container fluid className="resume-section bg-dark text-light">
         <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" , borderRadius: "20px"}}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download Resume
-          </Button>
+          <a className="button"  href={pdf} target="_blank" download rel="noreferrer">
+            <div className="button-wrapper">
+              <div className="text" variant="primary">
+                Download
+              </div>
+              <span className="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  role="img"
+                  width="2em"
+                  height="2em"
+                  preserveAspectRatio="xMidYMid meet"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 15V3m0 12l-4-4m4 4l4-4M2 17l.621 2.485A2 2 0 0 0 4.561 21h14.878a2 2 0 0 0 1.94-1.515L22 17"
+                  ></path>
+                </svg>
+              </span>
+            </div>
+          </a>
         </Row>
 
         <Row className="resume">
@@ -36,15 +54,33 @@ function ResumeNew() {
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px",borderRadius: "20px"}}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download Resume
-          </Button>
+          <a className="button"  href={pdf} target="_blank" download rel="noreferrer">
+            <div className="button-wrapper">
+              <div className="text" variant="primary">
+                Download
+              </div>
+              <span className="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  role="img"
+                  width="2em"
+                  height="2em"
+                  preserveAspectRatio="xMidYMid meet"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 15V3m0 12l-4-4m4 4l4-4M2 17l.621 2.485A2 2 0 0 0 4.561 21h14.878a2 2 0 0 0 1.94-1.515L22 17"
+                  ></path>
+                </svg>
+              </span>
+            </div>
+          </a>
         </Row>
       </Container>
     </div>
